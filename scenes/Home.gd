46c7a4d5d7ec17389e3ebf6104611fcef8000425
Antonio Lombardi <Antonio.Lombardi@eltako.de>
2023-04-20 21:50:26 +0200

    Add transition animation for start scene

--- scenes/Home.gd
+++ scenes/Home.gd
@@ -1,9 +1,18 @@
 extends Spatial
 
+onready var animation: AnimationPlayer = $AnimationPlayer
+onready var conductor: AudioStreamPlayer3D = $Conductor
+
 
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 func _on_StartButton_button_up():
+	animation.play("StartingAnimation")
+	if conductor.get_playback_position() < 36.0:
+		conductor.play(36.0)
+
+
+func start_level():
 	get_tree().change_scene("res://scenes/space.tscn")
