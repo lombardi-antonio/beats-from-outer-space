75de1ee7fb912b1a1b4aff8b88eb38c55b500af8
Antonio Lombardi <Antonio.Lombardi@eltako.de>
2023-06-05 17:11:56 +0200

    Add interactive loading for home scene

--- scenes/LoadingScreen.gd
+++ scenes/LoadingScreen.gd
@@ -3,11 +3,42 @@ extends Spatial
 onready var animation: AnimationPlayer = $AnimationPlayer
 
 var taped: bool = false
+var loader: ResourceInteractiveLoader = null
 var home_scene = null
+var wait_frames = 1
+var time_max = 100 # msec
 
 
 func _ready():
-	home_scene = load("res://scenes/Home.tscn")
+	loader = ResourceLoader.load_interactive("res://scenes/Home.tscn")
+
+
+func _process(_delta):
+	if loader == null:
+		# no need to process anymore
+		return
+
+	# Wait for frames to let the "loading" animation show up.
+	if wait_frames > 0:
+		wait_frames -= 1
+		return
+
+	var t = OS.get_ticks_msec()
+	# Use "time_max" to control for how long we block this thread.
+	while OS.get_ticks_msec() < t + time_max:
+		# Poll your loader.
+		var err = loader.poll()
+
+		if err == ERR_FILE_EOF: # Finished loading.
+			home_scene = loader.get_resource()
+			loader = null
+			animation.play("Rotate")
+			break
+		elif err == OK:
+			pass
+		else: # Error during loading.
+			loader = null
+			break
 
 
 func _input(event):
@@ -19,7 +50,7 @@ func _input(event):
 
 
 func goto_home():
-	if home_scene:
+	if home_scene != null:
 		return get_tree().change_scene_to(home_scene)
 	else:
 		return get_tree().change_scene("res://scenes/Home.tscn")
