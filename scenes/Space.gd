fdca11d009985ed45e47d96bcb2819a5f9fb6a2f
Antonio Lombardi <Antonio.Lombardi@eltako.de>
2023-06-13 18:29:48 +0200

    Create export variables needed for level change

--- scenes/Space.gd
+++ scenes/Space.gd
@@ -1,15 +1,22 @@
 extends Spatial
 
-export(Array, PackedScene) var spawner_list_level1
+export(Array, Array, PackedScene) var levels
+export(Array, AudioStreamMP3) var music_list
+export(Array, int) var music_list_bpm
 
 var time_scale: float
 var kills: int
 var points: int
 
-var _wave: int = 0
+var _current_level: int = 0
+var _current_wave: int = 0
 var _movement_disabled: bool
 var _ready_for_next_spawn: bool = false
 var _level_cleared: bool = false
+
+onready var conductor: AudioStreamPlayer3D = $Conductor
+onready var animation: AnimationPlayer = $AnimationPlayer
+onready var universe_mesh: MeshInstance = $UniverseMesh
 
 signal level_cleared()
 signal spawner_defeated()
@@ -21,11 +28,25 @@ func _ready():
 	_movement_disabled = false
 	_init_next_spawner()
 
+	if is_instance_valid(conductor):
+
+		if conductor.playing:
+			conductor.stop()
+
+		conductor.stream = music_list[1]
+		conductor.bpm = music_list_bpm[1]
+		conductor.recalculate_sec_per_beat()
+		conductor.play()
+
 
 func _init_next_spawner():
-	if _wave > spawner_list_level1.size() - 1:
-		return get_tree().reload_current_scene()
-	_spawn(spawner_list_level1[_wave])
+	if !levels:
+		return
+
+	if _current_level > levels.size():
+		goto_credits()
+
+	_spawn(levels[_current_level][_current_wave])
 
 
 func _spawn(spawner: PackedScene):
@@ -39,17 +60,25 @@ func _on_spawner_defeated():
 	emit_signal("spawner_defeated")
 	get_tree().call_group("projectile", "remove_self")
 
-	if _wave >= spawner_list_level1.size() - 1:
+	if _current_wave >= levels[_current_level].size() - 1:
 		emit_signal("level_cleared")
 		_level_cleared = true
+		_current_level += 1
+		_current_wave = 0
+		_transition_to_next_level()
 
-	_wave += 1
+	if _current_level >= levels.size():
+		goto_credits()
+
 	_ready_for_next_spawn = true
 
 
 func _on_vapor_falcon_was_defeated():
 	get_tree().call_group("enemies", "remove_self")
 	get_tree().call_group("projectile", "remove_self")
+
+	kills = 0
+	points = 0
 
 
 func _on_CameraBase_ready_for_next_spawn():
@@ -58,13 +87,19 @@ func _on_CameraBase_ready_for_next_spawn():
 	if not _ready_for_next_spawn:
 		return
 
-	elif _level_cleared:
-		goto_credits()
-
 	else:
 		get_tree().call_group("projectile", "remove_self")
 		_init_next_spawner()
 		_ready_for_next_spawn = false
+
+
+func _load_universe_image():
+	if is_instance_valid(universe_mesh):
+		universe_mesh.mesh.material.albedo_texture.image = load("res://sprites/Space_Stars" + String(_current_level + 1) +".png")
+
+
+func _transition_to_next_level():
+	animation.play("Transition")
 
 
 func goto_next_level():
