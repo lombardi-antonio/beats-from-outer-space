26ba59896fdcee574dfa99a19fd321f5f5718eb7
Antonio Lombardi <Antonio.Lombardi@eltako.de>
2023-08-15 16:07:52 +0200

    Adjust ufo parts

--- scenes/enemies/ufo/UfoHead.gd
+++ scenes/enemies/ufo/UfoHead.gd
@@ -2,25 +2,64 @@ extends Area
 
 
 export(bool) var open: bool = false
+export(int) var health: int = 200
 
+onready var collision: CollisionShape = $CollisionShape
 onready var animation: AnimationPlayer = $AnimationPlayer
+onready var animation_tree: AnimationTree = $AnimationTree
+onready var heart: MeshInstance = $HeartMesh
 
 
 func _ready():
-	pass
+	collision.disabled = true
 
 
 func _process(_delta):
 	_process_time_scale()
+	_reset_animation_parameters()
 
 
 func _process_time_scale():
-	animation.playback_speed = Space.time_scale
+	animation_tree["parameters/Blowback/TimeScale/scale"] = Space.time_scale
+	animation_tree["parameters/Defeated/TimeScale/scale"] = Space.time_scale
+	animation_tree["parameters/Idle/TimeScale/scale"] = Space.time_scale
+	animation_tree["parameters/Idle_Close/TimeScale/scale"] = Space.time_scale
+	animation_tree["parameters/Idle_Open/TimeScale/scale"] = Space.time_scale
+
+
+func _reset_animation_parameters():
+	animation_tree["parameters/conditions/is_opening"] = 0
+	animation_tree["parameters/conditions/is_closing"] = 0
+	animation_tree["parameters/conditions/is_hit"] = 0
 
 
 func open_hatch():
-	animation.play("Open")
+	animation_tree["parameters/conditions/is_opening"] = true
 
 
 func close_hatch():
-	animation.play("Closed")
+	animation_tree["parameters/conditions/is_closing"] = true
+
+
+func deal_damage(damage):
+	animation_tree["parameters/conditions/is_hit"] = true
+	health -= damage
+	if health <= 0:
+		animation_tree["parameters/conditions/is_defeated"] = true
+
+
+func _boss_defeated():
+	hide()
+	Space.kills += 1
+	Space.points += 1000
+	emit_signal("was_defeated")
+
+
+func _on_UfoHead_body_entered(body):
+	if body.name != 'VaporFalcon': return
+
+	body.deal_damage(1000)
+
+
+func _update_animation_parameters():
+	animation_tree["parameters/conditions/is_idle"] = true
---
+++ scenes/enemies/ufo/UfoTurret.gd
@@ -0,0 +1,70 @@
+extends Area
+
+export(PackedScene) var projectile
+export(int) var health: int = 50
+
+onready var collision: CollisionShape = $CollisionShape
+onready var animation_tree: AnimationTree = $AnimationTree
+
+var in_position: bool = false
+
+signal was_defeated()
+
+
+func _ready():
+	in_position = true
+
+
+func _process(_delta):
+	_process_time_scale()
+	_reset_animation_parameters()
+
+	if in_position:
+		animation_tree["parameters/conditions/is_shooting"] = 1
+
+
+func deal_damage(damage):
+	health -= damage
+	if health > 0:
+		animation_tree["parameters/conditions/is_hit"] = 1
+	else:
+		animation_tree["parameters/conditions/is_defeated"] = 1
+		if collision:
+			collision.queue_free()
+		Space.kills += 1
+		Space.points += 10
+		emit_signal("was_defeated")
+
+
+
+func _process_time_scale():
+	animation_tree["parameters/Idle/TimeScale/scale"] = Space.time_scale
+	animation_tree["parameters/Shooting/TimeScale/scale"] = Space.time_scale
+	animation_tree["parameters/Blowback/TimeScale/scale"] = Space.time_scale
+
+
+func _reset_animation_parameters():
+	animation_tree["parameters/conditions/is_shooting"] = 0
+	animation_tree["parameters/conditions/stoped_shooting"] = 0
+	animation_tree["parameters/conditions/is_hit"] = 0
+
+
+func _shoot():
+	if !is_instance_valid(projectile):
+		return
+
+	var new_projectile = projectile.instance()
+	get_tree().get_root().add_child(new_projectile)
+	new_projectile.translation = global_transform.origin + Vector3.UP * 0.3
+
+
+func _remove_self():
+	hide()
+	queue_free()
+
+
+func _on_UfoTurret_body_entered(body:Node):
+	if body.name != 'VaporFalcon': return
+
+	body.deal_damage(20)
+	deal_damage(50)
